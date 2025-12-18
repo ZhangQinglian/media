2a1d8d9cc3ef63f932bac797e6263c67f47564c6
zhangqinglian <zhangqinglian@migu.cn>
2025-12-19 00:08:10 +0800

    update to 1.9.0

--- libraries/session/src/main/java/androidx/media3/session/MediaSessionService.java
+++ libraries/session/src/main/java/androidx/media3/session/MediaSessionService.java
@@ -820,7 +820,7 @@ public abstract class MediaSessionService extends Service {
   public final void triggerNotificationUpdate() {
     List<MediaSession> sessions = getSessions();
     for (int i = 0; i < sessions.size(); i++) {
-      onUpdateNotificationInternal(sessions.get(i), /* startInForegroundWhenPaused= */ false);
+      onUpdateNotificationInternal(sessions.get(i), /* startInForegroundWhenPaused= */ false, NOTIFICATION_UPDATE_MANUAL);
     }
   }
 
